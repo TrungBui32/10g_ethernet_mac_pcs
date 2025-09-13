6573c99248aec5b63e1e8cad80edcafa063fa0ed
TrungBui32 <thetrung205@gmail.com>
2025-09-13 18:37:43 +0800

    reformat the control block code

--- src/encoder.v
+++ src/encoder.v
@@ -18,19 +18,21 @@ module encoder #(
     localparam SYNC_DATA = 2'b01;      // Data 
     localparam SYNC_CTRL = 2'b10;      // Control 
     
-    // Control block type field values (IEEE 802.3 Table 49-7)
-    localparam BLOCK_TYPE_C0 = 8'h1E;  // C0 C1 C2 C3 / C4 C5 C6 C7
-    localparam BLOCK_TYPE_S4 = 8'h33;  // C0 C1 C2 C3 / S4 D5 D6 D7
-    localparam BLOCK_TYPE_S0 = 8'h78;  // S0 D1 D2 D3 / D4 D5 D6 D7
-    localparam BLOCK_TYPE_T0 = 8'h87;  // T0 C1 C2 C3 / C4 C5 C6 C7
-    localparam BLOCK_TYPE_T1 = 8'h99;  // D0 T1 C2 C3 / C4 C5 C6 C7
-    localparam BLOCK_TYPE_T2 = 8'hAA;  // D0 D1 T2 C3 / C4 C5 C6 C7
-    localparam BLOCK_TYPE_T3 = 8'hB4;  // D0 D1 D2 T3 / C4 C5 C6 C7
-    localparam BLOCK_TYPE_T4 = 8'hCC;  // D0 D1 D2 D3 / T4 C5 C6 C7
-	localparam BLOCK_TYPE_T5 = 8'hD2;  // D0 D1 D2 D3 / D4 T5 C6 C7
-    localparam BLOCK_TYPE_T6 = 8'hE1;  // D0 D1 D2 D3 / D4 D5 T6 C7
-    localparam BLOCK_TYPE_T7 = 8'hFF;  // D0 D1 D2 D3 / D4 D5 D6 T7 
-    
+    // Control block type field values (IEEE 802.3 Table 49-7)  
+	localparam BLOCK_TYPE_C0 = 8'h1E;  // C0 C1 C2 C3 / C4 C5 C6 C7 => 11111111			
+	localparam BLOCK_TYPE_S4 = 8'h33;  // C0 C1 C2 C3 / S4 D5 D6 D7 => 00011111  
+	localparam BLOCK_TYPE_S0 = 8'h78;  // S0 D1 D2 D3 / D4 D5 D6 D7 => 00000001
+	localparam BLOCK_TYPE_T0 = 8'h87;  // T0 C1 C2 C3 / C4 C5 C6 C7 => 11111111			// duplicate, solve by checking T0
+	localparam BLOCK_TYPE_T1 = 8'h99;  // D0 T1 C2 C3 / C4 C5 C6 C7 => 11111110
+	localparam BLOCK_TYPE_T2 = 8'hAA;  // D0 D1 T2 C3 / C4 C5 C6 C7 => 11111100
+	localparam BLOCK_TYPE_T3 = 8'hB4;  // D0 D1 D2 T3 / C4 C5 C6 C7 => 11111000
+	localparam BLOCK_TYPE_T4 = 8'hCC;  // D0 D1 D2 D3 / T4 C5 C6 C7 => 11110000
+	localparam BLOCK_TYPE_T5 = 8'hD2;  // D0 D1 D2 D3 / D4 T5 C6 C7 => 11100000
+	localparam BLOCK_TYPE_T6 = 8'hE1;  // D0 D1 D2 D3 / D4 D5 T6 C7 => 11000000
+	localparam BLOCK_TYPE_T7 = 8'hFF;  // D0 D1 D2 D3 / D4 D5 D6 T7 => 10000000
+	
+	
+	
     localparam XGMII_IDLE = 8'h07;
     localparam XGMII_START = 8'hFB;
     localparam XGMII_TERMINATE = 8'hFD;
@@ -97,12 +99,17 @@ module encoder #(
             end else begin
                 encoded_data_out[65:64] <= SYNC_CTRL;
                 casez (xgmii_ctrl_block)
-                    8'b11111111: begin 
-                        encoded_data_out[63:56] <= BLOCK_TYPE_C0;
-                        encoded_data_out[55:0] <= {7{XGMII_IDLE}};
+                    8'b11111111: begin 			// C0 and T0
+						if(xgmii_data_block[7:0] == 8'hFD) begin 		// T0, 8'hFD == XGMII_TERMINATE
+							encoded_data_out[63:56] <= BLOCK_TYPE_T0;
+							encoded_data_out[55:0] <= {7{XGMII_IDLE}};
+						end else begin 									// C0
+							encoded_data_out[63:56] <= BLOCK_TYPE_C0;
+							encoded_data_out[55:0] <= {7{XGMII_IDLE}};
+						end
                     end
                     
-                    8'b00010000: begin 
+                    8'b00011111: begin 
                         encoded_data_out[63:56] <= BLOCK_TYPE_S4;
                         encoded_data_out[55:32] <= xgmii_data_block[31:8];
                         encoded_data_out[31:0] <= xgmii_data_block[63:32];
@@ -113,37 +120,37 @@ module encoder #(
                         encoded_data_out[55:0] <= xgmii_data_block[63:8];
                     end
                     
-                    8'b00000010: begin 
+                    8'b11111110: begin 
                         encoded_data_out[63:56] <= BLOCK_TYPE_T1;
                         encoded_data_out[55:48] <= xgmii_data_block[7:0];
                         encoded_data_out[47:0] <= {6{XGMII_IDLE}};
                     end
                     
-                    8'b00000100: begin 
+                    8'b11111100: begin 
                         encoded_data_out[63:56] <= BLOCK_TYPE_T2;
                         encoded_data_out[55:40] <= xgmii_data_block[15:0];
                         encoded_data_out[39:0] <= {5{XGMII_IDLE}};
                     end
                     
-                    8'b00001000: begin 
+                    8'b11111000: begin 
                         encoded_data_out[63:56] <= BLOCK_TYPE_T3;
                         encoded_data_out[55:32] <= xgmii_data_block[23:0];
                         encoded_data_out[31:0] <= {4{XGMII_IDLE}};
                     end
                     
-                    8'b00010000: begin 
+                    8'b11110000: begin 
                         encoded_data_out[63:56] <= BLOCK_TYPE_T4;
                         encoded_data_out[55:24] <= xgmii_data_block[31:0];
                         encoded_data_out[23:0] <= {3{XGMII_IDLE}};
                     end
                     
-                    8'b00100000: begin 
+                    8'b11100000: begin 
                         encoded_data_out[63:56] <= BLOCK_TYPE_T5;
                         encoded_data_out[55:16] <= xgmii_data_block[39:0];
                         encoded_data_out[15:0] <= {2{XGMII_IDLE}};
                     end
                     
-                    8'b01000000: begin
+                    8'b11000000: begin
                         encoded_data_out[63:56] <= BLOCK_TYPE_T6;
                         encoded_data_out[55:8] <= xgmii_data_block[47:0];
                         encoded_data_out[7:0] <= XGMII_IDLE;
