46804fee9778049a3d3fef2fccf8b59f40e859bf
TrungBui32 <thetrung205@gmail.com>
2025-09-25 13:33:14 +0800

    update encoder, works well

--- src/decoder.v
+++ src/decoder.v
@@ -3,12 +3,11 @@ module decoder #(
     parameter XGMII_DATA_WIDTH = 32,
     parameter XGMII_DATA_BYTES = XGMII_DATA_WIDTH/8
 ) (
-    input rx_clk,
-    input rx_rst,
+    input clk,
+    input rst,
     
     input [PCS_DATA_WIDTH-1:0] encoded_data_in,
     input encoded_valid_in,
-    output reg encoded_ready_out,
     
     output reg [XGMII_DATA_WIDTH-1:0] xgmii_data_out,
     output reg [XGMII_DATA_BYTES-1:0] xgmii_ctrl_out,
@@ -36,141 +35,134 @@ module decoder #(
     localparam XGMII_TERMINATE = 8'hFD;
     localparam XGMII_ERROR = 8'hFE;
     
-    reg [1:0] output_state;
-    localparam OUT_IDLE = 2'b00;
-    localparam OUT_LOW = 2'b01;
-    localparam OUT_HIGH = 2'b10;
+    reg state;
+    localparam FIRST = 1'b0;
+    localparam SECOND = 1'b1;
     
     reg [63:0] decoded_data;
     reg [7:0] decoded_ctrl;
     reg block_valid;
     reg decode_error;
-    
-    always @(*) begin
-        encoded_ready_out = (output_state == OUT_IDLE) || 
-                           (output_state == OUT_HIGH && xgmii_ready_in);
-    end
-    
-    always @(posedge rx_clk) begin
-        if (!rx_rst) begin
+            
+    always @(posedge clk) begin
+        if (!rst) begin
             decoded_data <= 64'h0;
             decoded_ctrl <= 8'h0;
             block_valid <= 1'b0;
             decode_error <= 1'b0;
-        end else if (encoded_valid_in && encoded_ready_out) begin
-            block_valid <= 1'b1;
-            decode_error <= 1'b0;
-            
-            case (encoded_data_in[65:64])
-                SYNC_DATA: begin
-                    decoded_data <= encoded_data_in[63:0];
-                    decoded_ctrl <= 8'h00; // All data
-                end
-                
-                SYNC_CTRL: begin
-                    case (encoded_data_in[63:56])
-                        BLOCK_TYPE_C0: begin
-                            decoded_data <= {{7{XGMII_IDLE}}, encoded_data_in[55:48]};
-                            decoded_ctrl <= 8'hFF; 
-                        end
-                        
-                        BLOCK_TYPE_S0: begin
-                            decoded_data <= {encoded_data_in[55:0], XGMII_START};
-                            decoded_ctrl <= 8'h01; 
-                        end
-                        
-                        BLOCK_TYPE_S4: begin
-                            decoded_data <= {encoded_data_in[31:0], XGMII_START, encoded_data_in[55:32]};
-                            decoded_ctrl <= 8'h10; 
-                        end
-                        
-                        BLOCK_TYPE_T0: begin
-                            decoded_data <= {{7{XGMII_IDLE}}, XGMII_TERMINATE};
-                            decoded_ctrl <= 8'hFF; 
-                        end
-                        
-                        BLOCK_TYPE_T1: begin
-                            decoded_data <= {{6{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:48]};
-                            decoded_ctrl <= 8'hFE; 
-                        end
-                        
-                        BLOCK_TYPE_T2: begin
-                            decoded_data <= {{5{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:40]};
-                            decoded_ctrl <= 8'hFC; 
-                        end
-                        
-                        BLOCK_TYPE_T3: begin
-                            decoded_data <= {{4{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:32]};
-                            decoded_ctrl <= 8'hF8; 
-                        end
-                        
-                        BLOCK_TYPE_T4: begin
-                            decoded_data <= {{3{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:24]};
-                            decoded_ctrl <= 8'hF0; 
-                        end
-                        
-                        BLOCK_TYPE_T5: begin
-                            decoded_data <= {{2{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:16]};
-                            decoded_ctrl <= 8'hE0;
-                        end
-                        
-                        BLOCK_TYPE_T6: begin
-                            decoded_data <= {XGMII_IDLE, XGMII_TERMINATE, encoded_data_in[55:8]};
-                            decoded_ctrl <= 8'hC0; 
-                        end
-                        
-                        BLOCK_TYPE_T7: begin
-                            decoded_data <= {XGMII_TERMINATE, encoded_data_in[55:0]};
-                            decoded_ctrl <= 8'h80; 
-                        end
-                        
-                        default: begin
-                            decoded_data <= {8{XGMII_ERROR}};
-                            decoded_ctrl <= 8'hFF;
-                            decode_error <= 1'b1;
-                        end
-                    endcase
-                end
-                
-                default: begin
-                    decoded_data <= {8{XGMII_ERROR}};
-                    decoded_ctrl <= 8'hFF;
-                    decode_error <= 1'b1;
-                end
-            endcase
         end else begin
             block_valid <= 1'b0;
+            decode_error <= 1'b0;
+            
+            if (encoded_valid_in) begin 
+                block_valid <= 1'b1;
+                case (encoded_data_in[65:64])
+                    SYNC_DATA: begin
+                        decoded_data <= encoded_data_in[63:0];
+                        decoded_ctrl <= 8'h00; // All data
+                    end
+                    
+                    SYNC_CTRL: begin
+                        case (encoded_data_in[63:56])
+                            BLOCK_TYPE_C0: begin
+                                decoded_data <= {{7{XGMII_IDLE}}, encoded_data_in[55:48]};
+                                decoded_ctrl <= 8'hFF; 
+                            end
+                            
+                            BLOCK_TYPE_S0: begin
+                                decoded_data <= {encoded_data_in[55:0], XGMII_START};
+                                decoded_ctrl <= 8'h01; 
+                            end
+                            
+                            BLOCK_TYPE_S4: begin
+                                decoded_data <= {encoded_data_in[31:0], XGMII_START, encoded_data_in[55:32]};
+                                decoded_ctrl <= 8'h10; 
+                            end
+                            
+                            BLOCK_TYPE_T0: begin
+                                decoded_data <= {{7{XGMII_IDLE}}, XGMII_TERMINATE};
+                                decoded_ctrl <= 8'hFF; 
+                            end
+                            
+                            BLOCK_TYPE_T1: begin
+                                decoded_data <= {{6{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:48]};
+                                decoded_ctrl <= 8'hFE; 
+                            end
+                            
+                            BLOCK_TYPE_T2: begin
+                                decoded_data <= {{5{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:40]};
+                                decoded_ctrl <= 8'hFC; 
+                            end
+                            
+                            BLOCK_TYPE_T3: begin
+                                decoded_data <= {{4{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:32]};
+                                decoded_ctrl <= 8'hF8; 
+                            end
+                            
+                            BLOCK_TYPE_T4: begin
+                                decoded_data <= {{3{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:24]};
+                                decoded_ctrl <= 8'hF0; 
+                            end
+                            
+                            BLOCK_TYPE_T5: begin
+                                decoded_data <= {{2{XGMII_IDLE}}, XGMII_TERMINATE, encoded_data_in[55:16]};
+                                decoded_ctrl <= 8'hE0;
+                            end
+                            
+                            BLOCK_TYPE_T6: begin
+                                decoded_data <= {XGMII_IDLE, XGMII_TERMINATE, encoded_data_in[55:8]};
+                                decoded_ctrl <= 8'hC0; 
+                            end
+                            
+                            BLOCK_TYPE_T7: begin
+                                decoded_data <= {XGMII_TERMINATE, encoded_data_in[55:0]};
+                                decoded_ctrl <= 8'h80; 
+                            end
+                            
+                            default: begin
+                                decoded_data <= {8{XGMII_ERROR}};
+                                decoded_ctrl <= 8'hFF;
+                                decode_error <= 1'b1;
+                            end
+                        endcase
+                    end
+                    
+                    default: begin
+                        decoded_data <= {8{XGMII_ERROR}};
+                        decoded_ctrl <= 8'hFF;
+                        decode_error <= 1'b1;
+                    end
+                endcase
+            end
         end
     end
     
-    always @(posedge rx_clk) begin
-        if (!rx_rst) begin
-            output_state <= OUT_IDLE;
+    always @(posedge clk) begin
+        if (!rst) begin
+            state <= FIRST;
             xgmii_data_out <= {XGMII_DATA_WIDTH{1'b0}};
             xgmii_ctrl_out <= {XGMII_DATA_BYTES{1'b0}};
             xgmii_valid_out <= 1'b0;
         end else begin
-            case (output_state)
-                OUT_IDLE: begin
+            case (state)
+                FIRST: begin
                     xgmii_valid_out <= 1'b0;
                     if (block_valid && xgmii_ready_in) begin
                         xgmii_data_out <= decoded_data[31:0];
                         xgmii_ctrl_out <= decoded_ctrl[3:0];
                         xgmii_valid_out <= 1'b1;
-                        output_state <= OUT_HIGH;
+                        state <= SECOND;
                     end
                 end
                 
-                OUT_HIGH: begin
-                    if (xgmii_ready_in) begin
-                        xgmii_data_out <= decoded_data[63:32];
-                        xgmii_ctrl_out <= decoded_ctrl[7:4];
-                        xgmii_valid_out <= 1'b1;
-                        output_state <= OUT_IDLE;
-                    end
+                SECOND: begin
+                    xgmii_data_out <= decoded_data[63:32];
+                    xgmii_ctrl_out <= decoded_ctrl[7:4];
+                    xgmii_valid_out <= 1'b1;
+                    state <= FIRST;
                 end
                 
-                default: output_state <= OUT_IDLE;
+                default: state <= FIRST;
             endcase
         end
     end
